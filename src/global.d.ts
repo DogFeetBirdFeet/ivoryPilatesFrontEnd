97b361bc34d10073569a78214c17e0bcd4897324
egoist89 <egoist89@naver.com>
2025-09-27 16:09:37 +0900

    로그인화면 진행중_김용진_250927

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,4 +5,5 @@ declare module 'framer-motion' {
   export type HTMLMotionUlProps = React.HTMLAttributes<HTMLUListElement> & MotionProps;
   export type HTMLMotionAsideProps = React.HTMLAttributes<HTMLElement> & MotionProps;
   export type HTMLMotionDivProps = React.HTMLAttributes<HTMLDivElement> & MotionProps;
+  export type HTMLMotionSvgProps = React.HTMLAttributes<HTMLDivElement> & MotionProps;
 }
