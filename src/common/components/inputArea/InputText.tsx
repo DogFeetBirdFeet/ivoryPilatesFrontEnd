d0c1c2e79961aefbf080bd6acf8ea682ff475402
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2025-09-29 14:01:18 +0900

    InputText 디자인클래스 상수로 수정

--- src/common/components/inputArea/InputText.tsx
+++ src/common/components/inputArea/InputText.tsx
@@ -1,3 +1,5 @@
+import { INPUT_BASE_CLASS } from './styleConstants';
+
 interface IArea {
   id: string;
   placeholder?: string;
@@ -14,11 +16,7 @@ interface IArea {
 export default function InputText({ id, className, placeholder }: IArea) {
   return (
     <>
-      <input
-        className={`w-[250px] leading-[30px] h-30px p-10px rounded-default bg-white border-[1px] border-gray text-base focus:outline-none focus:border-yellow ${className}`}
-        id={id}
-        aria-placeholder={placeholder}
-      />
+      <input className={`${INPUT_BASE_CLASS} ${className}`} id={id} aria-placeholder={placeholder} />
     </>
   );
 }
---
+++ src/common/components/inputArea/styleConstants.ts
@@ -0,0 +1,2 @@
+export const INPUT_BASE_CLASS =
+  'w-[250px] leading-[30px] h-30px p-10px rounded-[7px] bg-white border-[1px] border-gray text-sm focus:outline-none focus:border-yellow';
